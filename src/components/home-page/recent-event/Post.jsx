65241bdb741a1ffcc1c811615faf8f178f3b2c8f
RinWasBusted <thaian0609asd@gmail.com>
2025-08-13 14:16:21 +0700

    Update component: RecentPost

--- src/components/home-page/recent-event/Post.jsx
+++ src/components/home-page/recent-event/Post.jsx
@@ -1,16 +1,16 @@
 export default function Post({ event }) {
     return (
-        <div className="w-full min-h-55 flex sm:flex-row flex-col gap-5 p-5 border-1 rounded-xl border-accent shadow-2xs group cursor-pointer">
-            <figure className="h-55 flex-1 overflow-hidden">
+        <div className="w-full min-h-30 md:min-h-55 flex flex-col sm:flex-row gap-5 md:p-5 md:border-1 rounded-xl border-accent shadow-2xs group cursor-pointer">
+            <figure className="h-30 md:h-55 flex-1/3 overflow-hidden">
                 <img src={event.banner} alt="event banner" className="h-full object-cover group-hover:scale-110 duration-100" />
             </figure>
 
-            <article className="h-full flex-1 flex flex-col gap-3">
-                <p className="text-accent text-[14px] font-semibold">{event.progress}</p>
+            <article className="h-full flex-2/3 flex flex-col gap-1 md:gap-3">
+                <p className="text-accent text-[10px] md:text-[14px] font-semibold">{event.progress}</p>
 
-                <h3 className="text-[18px] font-semibold hover:underline">{event.title}</h3>
+                <h3 className=" text-[15px] md:text-[18px] font-semibold hover:underline">{event.title}</h3>
 
-                <p className="text-[16px] opacity-70">{event.desc}</p>
+                <p className=" text-[10px] md:text-[16px] opacity-70">{event.desc}</p>
 
                 <div className="flex gap-2 mt-auto">
                     {event.tags.map((tag, index) => <Tag key={index} content={tag}></Tag>)}
@@ -22,7 +22,7 @@ export default function Post({ event }) {
 
 const Tag = ({ content }) => {
     return (
-        <div className="text-primary bg-secondary h-6 text-[14px] font-[500] px-3 flex justify-center items-center rounded-[12px]">
+        <div className="text-primary bg-secondary h-3 md:h-6 text-[10px] md:text-[14px] font-[500] px-3 flex justify-center items-center rounded-[12px]">
             {content}
         </div>
     )
